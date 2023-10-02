abbd9c061fabf7932890248dd2ef99a18207bd4f
mservillon <mackservillon@gmail.com>
2023-10-02 13:41:34 -0500

    useState() review and react state

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -1,16 +1,36 @@
 import Button from '@mui/material/Button'
+import { FC, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
-export const Setup = () => {
+interface SetupProps {
+  num: number;
+  setNum: any;
+}
 
+export const Setup: FC<SetupProps> = ({num, setNum}) => {
+
+  console.log("Setup called")
   const navigate = useNavigate();
+
+  // let num = 1;
+  // const [num, setNum] = useState(1);
 
     return (
       <>
         <h3>
-         Setup a Game
+         Setup a Game ({num})
         </h3>
-        <Button variant="outlined" size="large" onClick={() => navigate('/play')}>
+        <Button variant="outlined" size="large" 
+        onClick={
+          // () => navigate('/play')
+          () => {
+            // setNum(num + 1);
+            // console.log(num);
+
+            setNum(num + 1)
+            navigate('/play');
+          }
+        }>
           Start Game
         </Button>
       </>
